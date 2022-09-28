e76f3664c1162250f6e0a647965f66a1d97dc40c
Ayoubon <aarrasseayoub@gmail.com>
2022-09-28 12:25:26 +0100

    fix mongodb

--- server/index.js
+++ server/index.js
@@ -74,8 +74,8 @@ app.use("/api/raw", RawRouter);
 app.use("/api/personnel", PersonnelRouter);
 app.use("/api/zone", ZoneRouter);
 app.use("/api/equipement", EquipementRouter);
-const CONNECTION_URL= 'mongodb+srv://a:a@cluster0.jdx5hmo.mongodb.net/?retryWrites=true&w=majority';
+const CONNECTION_URL= 'mongodb://qhse:qhse@ac-8iihps4-shard-00-00.jdx5hmo.mongodb.net:27017,ac-8iihps4-shard-00-01.jdx5hmo.mongodb.net:27017,ac-8iihps4-shard-00-02.jdx5hmo.mongodb.net:27017/?ssl=true&replicaSet=atlas-6frsib-shard-0&authSource=admin&retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true}).then(()=> app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`))).catch((error) => console.log(error.message));
+mongoose.connect(CONNECTION_URL).then(()=> app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`))).catch((error) => console.log(error.message));
 
